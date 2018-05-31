11b1871094a385476f19e0b672c6b7c2fd2f3b39
Antonio Nogueras <antonio@square205.com>
2018-05-31 14:14:07 -0500

    changed border size

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ chrome.storage.local.get({
     if(data.onOff === 'on') {
       x = document.querySelectorAll('*')
       for (var i = x.length - 1; i >= 0; i--) {
-        x[i].style.border = '1px solid red';
+        x[i].style.border = '0.2px solid red';
      
     }
 
