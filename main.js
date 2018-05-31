2996bb1d615b3ef11ad921b5ede1b9192a8c0c88
Antonio Nogueras <antonio@square205.com>
2018-05-30 19:29:54 -0500

    created project. built functionality

---
+++ main.js
@@ -0,0 +1,15 @@
+chrome.storage.local.get({
+    onOff: 'off'
+}, function(data) {
+    if(data.onOff === 'on') {
+      x = document.querySelectorAll('*')
+      for (var i = x.length - 1; i >= 0; i--) {
+        x[i].style.border = '1px solid red';
+     
+    }
+
+    }
+});
+
+
+
